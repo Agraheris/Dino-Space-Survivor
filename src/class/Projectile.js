996a5d66c2c38af8fb1ef9a74f6bba8732dc9a62
Your Name <ewan16270409@outlook.fr>
2024-10-03 09:24:07 +0200

    :sparkles: Add attack enemies

---
+++ src/class/Projectile.js
@@ -0,0 +1,33 @@
+import Phaser from "phaser";
+
+export class Projectile extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, startX, startY, projectileImage) {
+    super(scene, startX, startY, projectileImage);
+
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+    this.body.setAllowGravity(false);
+    this.speed = 300; // Vitesse du projectile
+  }
+
+  // Positionnement et direction du projectile
+  shoot(startX, startY, directionX, directionY) {
+    this.setPosition(startX, startY);
+    this.setActive(true);
+    this.setVisible(true);
+
+    this.setVelocity(directionX * this.speed, directionY * this.speed);
+  }
+
+  update() {
+    // Détruire le projectile si il sort de l'écran
+    if (
+      this.x < 0 || // Trop à gauche
+      this.x > this.scene.sys.canvas.width || // Trop à droite
+      this.y < 0 || // Trop en haut
+      this.y > this.scene.sys.canvas.height // Trop en bas
+    ) {
+      this.destroy();
+    }
+  }
+}
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,28 +1,61 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
+import { Projectile } from "../class/Projectile";
 
-export class Game extends Scene
-{
-    constructor ()
-    {
-        super('Game');
-    }
+export class Game extends Scene {
+  constructor() {
+    super("Game");
+  }
 
-    create ()
-    {
-        this.cameras.main.setBackgroundColor(0x00ff00);
+  create() {
+    this.cameras.main.setBackgroundColor(0x00ff00);
+    this.add.image(512, 384, "background").setAlpha(0.5);
 
-        this.add.image(512, 384, 'background').setAlpha(0.5);
+    // Gérer plusieurs projectiles
+    this.projectiles = this.physics.add.group({
+      classType: Projectile,
+      runChildUpdate: true, // Met à jour les projectiles avec Update
+    });
 
-        this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+    this.enemies = [
+      {
+        enemy: this.physics.add.sprite(300, 300, "enemy"),
+        directions: [
+          [-1, 0], // Tire à gauche
+          [0, 1], // Tire vers le bas
+          [-1, -1], // Tire en diagonale haut-gauche
+        ],
+      },
+      {
+        enemy: this.physics.add.sprite(700, 300, "enemy"),
+        directions: [
+          [1, 0], // Tire à droite
+          [0, -1], // Tire vers le haut
+          [1, 1], // Tire en diagonale bas-droite
+        ],
+      },
+    ];
 
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('GameOver');
-
+    // Tir automatique des ennemis
+    this.enemies.forEach(({ enemy, directions }) => {
+      directions.forEach((direction) => {
+        this.time.addEvent({
+          delay: 2000, // Tir toutes les 2 secondes
+          callback: () =>
+            this.shootProjectileFromEnemy(enemy, direction[0], direction[1]),
+          callbackScope: this,
+          loop: true, // Répéter l'événement
         });
+      });
+    });
+  }
+
+  // Direction du tir
+  shootProjectileFromEnemy(enemy, directionX, directionY) {
+    const projectile = this.projectiles.get();
+    if (projectile) {
+      projectile.shoot(enemy.x, enemy.y, directionX, directionY); // Lance le projectile
     }
+  }
+
+  update() {}
 }
