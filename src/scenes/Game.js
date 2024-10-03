fb83a966243258f10b9a58bf3f7c9f3ba5287e84
Your Name <ewan16270409@outlook.fr>
2024-10-03 11:44:23 +0200

    :sparkles: Add random eneùies in card with random shoots

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -7,9 +7,15 @@ export class Game extends Scene {
     super("Game");
   }
 
+  preload() {
+    this.load.image("background-stars", "assets/background_stars.webp");
+  }
+
   create() {
-    this.cameras.main.setBackgroundColor(0x00ff00);
-    this.add.image(512, 384, "background").setAlpha(0.5);
+    // Pour que l'image prenne toute l'écran
+    const { width, height } = this.scale;
+    const background = this.add.image(0, 0, "background-stars").setOrigin(0, 0);
+    background.setDisplaySize(width, height);
 
     // Gérer plusieurs projectiles
     this.projectiles = this.physics.add.group({
@@ -17,43 +23,58 @@ export class Game extends Scene {
       runChildUpdate: true, // Met à jour les projectiles avec Update
     });
 
-    this.enemies = [
-      {
-        enemy: this.physics.add.sprite(300, 300, "enemy"),
-        directions: [
-          [-1, 0], // Tire à gauche
-          [0, 1], // Tire vers le bas
-          [-1, -1], // Tire en diagonale haut-gauche
-        ],
-      },
-      {
-        enemy: this.physics.add.sprite(700, 300, "enemy"),
-        directions: [
-          [1, 0], // Tire à droite
-          [0, -1], // Tire vers le haut
-          [1, 1], // Tire en diagonale bas-droite
-        ],
-      },
-    ];
-
-    // Tir automatique des ennemis
-    this.enemies.forEach(({ enemy, directions }) => {
-      directions.forEach((direction) => {
-        this.time.addEvent({
-          delay: 2000, // Tir toutes les 2 secondes
-          callback: () =>
-            this.shootProjectileFromEnemy(enemy, direction[0], direction[1]),
-          callbackScope: this,
-          loop: true, // Répéter l'événement
-        });
-      });
-    });
+    this.enemyGroup = this.physics.add.group();
 
     // Initialiser le joueur
     this.player = new Player(this, 400, 300, "player");
     this.input.once("pointerdown", () => {
       this.scene.start("GameOver");
       this.player.play("walk");
+    });
+
+    // Réapparition des ennemis
+    this.time.addEvent({
+      delay: 5000, // 5 secondes
+      callback: this.spawnEnemies,
+      callbackScope: this,
+      loop: true, // Répéter l'événement
+    });
+  }
+
+  // Créer de nouveaux ennemis
+  spawnEnemies() {
+    const { width, height } = this.scale;
+
+    const randomX = Phaser.Math.Between(0, width);
+    const randomY = Phaser.Math.Between(0, height);
+
+    const directions = [
+      [-1, 0], // Tire à gauche
+      [1, 0], // Tire à droite
+      [0, -1], // Tire vers le haut
+      [0, 1], // Tire vers le bas
+      [-1, -1], // Diagonale haut-gauche
+      [1, 1], // Diagonale bas-droite
+    ];
+
+    const enemy = this.enemyGroup.create(randomX, randomY, "enemy");
+
+    // Tirer dans la direction aléatoire
+    const randomIndex = Phaser.Math.Between(0, directions.length - 1);
+    const randomDirection = directions[randomIndex];
+
+    // Configuration Ennemis
+    this.time.addEvent({
+      delay: 2000, // Tir toutes les 2 secondes
+      callback: () => {
+        this.shootProjectileFromEnemy(
+          enemy,
+          randomDirection[0],
+          randomDirection[1]
+        );
+      },
+      callbackScope: this,
+      loop: true, // Répéter l'événement
     });
   }
 
